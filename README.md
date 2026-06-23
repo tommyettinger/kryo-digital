07c4b805ec15ad9141698ac2b64487ee6d95bfd0
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-22 19:41:48 -0700

    Bring over kryo-more code; new config.

---
+++ README.md
@@ -0,0 +1,19 @@
+# kryo-digital
+Kryo 5.x support for classes from [digital](https://github.com/tommyettinger/digital)
+
+## Get it
+
+Use JitPack.io as your repository (which gdx-liftoff projects already can do) and depend on:
+```
+implementation "com.github.tommyettinger:kryo-digital:0.10.2.2"
+```
+
+This depends on Kryo 5.6.2 .
+
+## Older Versions
+
+See [kryo-more](https://github.com/tommyettinger/kryo-more) for older releases.
+
+## License
+
+[Apache 2.0](LICENSE).
---
+++ src/main/java/com/github/tommyettinger/kryo/digital/BaseSerializer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.digital;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.digital.Base;
+
+/**
+ * Kryo {@link Serializer} for digital {@link Base}s.
+ */
+public class BaseSerializer extends Serializer<Base> {
+
+    public BaseSerializer() {
+        setImmutable(true);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final Base data) {
+        output.writeString(data.serializeToString());
+    }
+
+    @Override
+    public Base read(final Kryo kryo, final Input input, final Class<? extends Base> dataClass) {
+        return Base.deserializeFromString(input.readString());
+    }
+}
